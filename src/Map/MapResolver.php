af598b976daad47f8ef0a35ea462c1444a83e6c5
Taniguchi Yoshihiro <taniguchi@webware.co.jp>
2015-02-07 20:53:06 +0900

    テストをデータプロバイダを使うように修正。一部、無駄を省くコード追加。

--- src/Map/MapResolver.php
+++ src/Map/MapResolver.php
@@ -13,15 +13,40 @@ namespace nagoyaphp\doukaku8\Map;
  * 評価する人そのもの。
  *
  */
+/**
+ * Class MapResolver
+ * @package nagoyaphp\doukaku8\Map
+ */
 class MapResolver {
+
+    /**
+     * 外部から実行する評価を算出するメソッド
+     * 内部的にestimateOneを呼び出し、再帰的に処理を行う。
+     *
+     * @param Map $map
+     */
     public function estimate(Map $map)
     {
         foreach($map->getPoints() as $point)
         {
+            //端っこでどこにも行けないやつは評価外。
+            /* @var $point Point */
+            if($point->isEnd())
+            {
+                continue;
+            }
             $this->estimateOne($map, $point);
         }
     }
 
+    /**
+     * 自分の上下左右の値が大きければそのマス（Point)の評価値を上げていく。
+     * 再帰的に処理をする。
+     * 戻り値として最後のポイントを返す。
+     *
+     * @param Map $map
+     * @param Point $start_point
+     */
     private function estimateOne(Map $map, Point &$start_point)
     {
         //次がある場合は次のポイントの評価値を自分の評価値+1する。
@@ -37,10 +62,15 @@ class MapResolver {
                 if($next_point->getEstimate() < $start_point->getEstimate() + 1)
                 {
                     $next_point->setEstimate($start_point->getEstimate() + 1);
+
+                    //戻り値として最終到達点を設定
                     $this->estimateOne($map, $next_point);
                 }
             }
         }
-
+        else
+        {
+            $start_point->setEnd(true);
+        }
     }
 }
--- src/Map/Point.php
+++ src/Map/Point.php
@@ -12,13 +12,42 @@ namespace nagoyaphp\doukaku8\Map;
 * マスは評価値を持つ。
 * 上下左右のつながりから、評価値をMapResolverが設定する。
 */
+/**
+ * Class Point
+ * @package nagoyaphp\doukaku8\Map
+ */
 class Point {
 
+    /**
+     * マスのID
+     * @var int
+     */
     private $id;
+    /**
+     * 縦
+     * @var
+     */
     private $row;
+    /**
+     * 横
+     * @var
+     */
     private $col;
+    /**
+     * マスの値
+     * @var
+     */
     private $value;
+    /**
+     * 評価値
+     * @var int
+     */
     private $estimate = 1;
+    /**
+     * 何処にも行く事の無いマスの場合はtrueにする。
+     * @var bool
+     */
+    private $end=false;
 
     /**
      * @param $row
@@ -79,6 +108,22 @@ class Point {
     public function getValue()
     {
         return $this->value;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isEnd()
+    {
+        return $this->end;
+    }
+
+    /**
+     * @param boolean $end
+     */
+    public function setEnd($end)
+    {
+        $this->end = $end;
     }
 
 }
